966951cca7f46445eb09e190bef27f51ad51a95f
rash-123 <vermarashmi559@gmail.com>
2023-11-19 15:43:30 +0530

    file

--- EvenOdd.java
+++ EvenOdd.java
@@ -5,9 +5,9 @@ public class EvenOdd {
 	public static void main(String[] args) {
 		
 		Scanner s = new Scanner(System.in);
-		int num;
+		int num = 4;
 		System.out.println("enter the no");
-		num = s.nextInt();
+		
 		
 		String res = (num % 2 == 0) ? "even" : "odd";
 		
